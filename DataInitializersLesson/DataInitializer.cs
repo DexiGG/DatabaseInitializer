78cf97c3870661dff4785d20dcf15c2f1d975567
Джабашев Нурбек Кадербекович <@corp.itstep>
2019-04-25 21:29:02 +0600

    DataBaseInitializer

---
+++ DataInitializersLesson/DataInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInitializersLesson
+{
+    public class DataInitializer : DropCreateDatabaseAlways<CitiesContext>
+    {
+        protected override void Seed(CitiesContext context)
+        {
+            context.Cities.AddRange(new List<City>
+            {
+
+                new City
+                {
+                    Name = "Нур-Султан",
+                    Population = 1200000
+                },
+                   new City
+                {
+                    Name = "Алматы",
+                    Population = 1600000
+                },
+                      new City
+                {
+                    Name = "Стамбул",
+                    Population = 2000000
+                },
+               
+            });
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DataInitializersLesson/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInitializersLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new CitiesContext())
+            {
+                //context.Cities.Add(new City { Name = "Бишкек", Population = 1311231 });
+                //context.SaveChanges();
+                foreach(var city in context.Cities.ToList())
+                {
+                    Console.WriteLine(city.Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
